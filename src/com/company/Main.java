71390805d5b529115563855a71b5bd691533c56f
BaSmachi <narbaev2394@gmail.com>
2022-02-02 20:59:34 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Person beksultan = new Person();
+        beksultan.name = "Beksultan";
+        beksultan.lastName = "Sultanovich";
+        beksultan.age = 18;
+        beksultan.gender = " M";
+        beksultan.education = true;
+        beksultan.height = 1.80;
+        beksultan.weight = 66;
+        beksultan.run();
+
+        Person nursultan = new Person();
+        nursultan.name = "Sultann";
+        nursultan.lastName = "Ulanb ek uulu";
+        nursultan.age = 21;
+        nursultan.gender = " M";
+        nursultan.education = true;
+        nursultan.height = 1.80;
+        nursultan.weight = 66;
+        nursultan.run();
+        System.out.println();
+
+
+
+    }
+}
---
+++ src/com/company/Person.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Person {
+    String name;
+    String lastName;
+    int age;
+    double weight;
+    double height;
+    String gender;
+    String staus;
+    boolean education;
+
+    public void run() {
+        System.out.println(name + " is running " + age+gender);
+    }
+    public void speak(){
+        System.out.println(name+lastName);
+    }
+
+
+}
